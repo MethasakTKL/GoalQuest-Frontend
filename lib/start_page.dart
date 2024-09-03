b3b4d42024a4e43dbf7ea9c7a7fa767c8283783f
MethasakTKL <methasak.tkl@outlook.com>
2024-09-04 04:18:39 +0700

    UI Start Page

--- lib/start_page.dart
+++ lib/start_page.dart
@@ -1,19 +1,75 @@
 import 'package:flutter/material.dart';
 
 class StartPage extends StatelessWidget {
+  const StartPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Start Page'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/login');
-          },
-          child: Text("Let's Start"),
-        ),
+      backgroundColor: const Color.fromARGB(255, 19, 19, 19),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 200), // ปรับตำแหน่งตามต้องการ
+            child: Center(
+              child: Image.asset(
+                'assets/logo_white.png',
+                fit: BoxFit.contain,
+                height: 300,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 50),
+            child: Column(
+              children: [
+                const Text(
+                  'Welcome to GoalQuest',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  'Your Journey to Success Begins Here!\n'
+                  'Set goals, track progress, and earn rewards.\n'
+                  'Start your journey today!',
+                  style: TextStyle(
+                    color: Colors.white70,
+                    fontSize: 16,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 60),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/login');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 255, 166, 33),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 30, vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: const Text(
+                    "Let's Start",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
