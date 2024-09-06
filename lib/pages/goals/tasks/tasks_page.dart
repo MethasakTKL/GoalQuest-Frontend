2f62e99edb1245bbd392c6927e28fe3212fe6dd6
MethasakTKL <methasak.tkl@outlook.com>
2024-09-06 12:59:44 +0700

    Add tab in Tasks page

--- lib/pages/goals/tasks/tasks_page.dart
+++ lib/pages/goals/tasks/tasks_page.dart
@@ -6,46 +6,47 @@ class TasksPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-      appBar: AppBar(
+    return DefaultTabController(
+      length: 2, // จำนวนแท็บ
+      child: Scaffold(
         backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-        title: Row(
-          children: [
-            Image.asset(
-              'assets/logo_black.png',
-              height: 50,
-            ),
-            const Spacer(), // ใช้ Spacer เพื่อจัดตำแหน่ง
-            IconButton(
-              icon: const Icon(Icons.account_circle),
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const BottomNavigationPage(
-                      initialIndex: 3,
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+          title: Row(
+            children: [
+              Image.asset(
+                'assets/logo_black.png',
+                height: 50,
+              ),
+              const Spacer(),
+              IconButton(
+                icon: const Icon(Icons.account_circle),
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const BottomNavigationPage(
+                        initialIndex: 3,
+                      ),
                     ),
-                  ),
-                );
-              },
-              iconSize: 30,
-            ),
-          ],
+                  );
+                },
+                iconSize: 30,
+              ),
+            ],
+          ),
+          automaticallyImplyLeading: false,
         ),
-        automaticallyImplyLeading: false, // ปิดปุ่ม Back
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
-          child: SingleChildScrollView(
-            // เพิ่อให้สามารถเลื่อนหน้าจอได้ถ้าคอนเทนต์ใหญ่เกิน
-            child: Center(
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      IconButton(
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+            child: SingleChildScrollView(
+              child: Center(
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        IconButton(
                           icon: const Icon(
                             Icons.arrow_back,
                             color: Colors.black,
@@ -53,30 +54,63 @@ class TasksPage extends StatelessWidget {
                           ),
                           onPressed: () {
                             Navigator.pop(context);
-                          }),
-                      const SizedBox(width: 5),
-                      const Text(
-                        'Goal Title 1',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+                          },
                         ),
-                      ),
-                      const Spacer(),
-                      const Text(
-                        'New Task',
-                        style: TextStyle(
-                          fontSize: 14,
+                        const SizedBox(width: 5),
+                        const Text(
+                          'Goal Title 1',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
+                        const Spacer(),
+                        const Text(
+                          'New Task',
+                          style: TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.add_circle),
+                          onPressed: () {},
+                          iconSize: 40,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 0),
+                    const TabBar(
+                      labelColor: Colors.black,
+                      indicatorColor: Color.fromARGB(255, 95, 148, 80),
+                      tabs: [
+                        Tab(text: 'TodoQuest'),
+                        Tab(text: 'FocusTimer'),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    const SizedBox(
+                      height: 400,
+                      child: TabBarView(
+                        children: [
+                          // เนื้อหาในแท็บ Todoquest
+                          Center(
+                            child: Text(
+                              'Todoquest Content',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                          // เนื้อหาในแท็บ FocusTimer
+                          Center(
+                            child: Text(
+                              'FocusTimer Content',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                        ],
                       ),
-                      IconButton(
-                        icon: const Icon(Icons.add_circle),
-                        onPressed: () {},
-                        iconSize: 40,
-                      ),
-                    ],
-                  ),
-                ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
