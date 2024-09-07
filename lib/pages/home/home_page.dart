fd3b13cc8202c4a61bbf1aab0789c4f7186093be
MethasakTKL <methasak.tkl@outlook.com>
2024-09-07 23:33:56 +0700

    Slide show in home page

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:goal_quest/bottom_navigationbar/navigation_page.dart';
 import 'package:goal_quest/pages/goals/goals_list.dart';
 import 'package:goal_quest/pages/home/routing_panel.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -35,8 +36,7 @@ class HomePage extends StatelessWidget {
                   PageRouteBuilder(
                     pageBuilder: (context, animation1, animation2) =>
                         const BottomNavigationPage(initialIndex: 3),
-                    transitionDuration: const Duration(
-                        seconds: 0), // กำหนดเวลาของการเปลี่ยนหน้า
+                    transitionDuration: const Duration(seconds: 0),
                   ),
                 );
               },
@@ -44,7 +44,7 @@ class HomePage extends StatelessWidget {
             ),
           ],
         ),
-        automaticallyImplyLeading: false, // ปิดปุ่ม Back
+        automaticallyImplyLeading: false,
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -53,81 +53,83 @@ class HomePage extends StatelessWidget {
             child: Column(
               children: [
                 const Text("Welcome back, Steven"),
-                const SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  width: double.infinity,
-                  height: 170,
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(156, 255, 160, 8),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Stack(
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(
-                            20), // ปรับขอบให้มนเหมือน container
-                        child: Image.asset(
-                          'assets/slide_1.png',
-                          fit: BoxFit.cover, // ขยายรูปให้เต็ม container
-                          width: double.infinity,
-                          height: double.infinity,
+                const SizedBox(height: 10),
+                // เริ่ม Stack สำหรับ CarouselSlider และคะแนน 500 Points
+                Stack(
+                  children: [
+                    // CarouselSlider
+                    Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20), // ทำขอบโค้ง
+                      ),
+                      clipBehavior: Clip.antiAlias, // ให้คลิปภาพตามขอบที่โค้ง
+                      child: CarouselSlider(
+                        items: [
+                          Image.asset(
+                            'assets/slide_1.png',
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                          ),
+                          Image.asset(
+                            'assets/slide_2.png',
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                          ),
+                          Image.asset(
+                            'assets/slide_3.png',
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                          ),
+                        ],
+                        options: CarouselOptions(
+                          height: 170,
+                          autoPlay: true,
+                          enlargeCenterPage: true,
+                          aspectRatio: 16 / 9,
+                          autoPlayInterval: const Duration(seconds: 10),
+                          viewportFraction: 1.0,
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 10, right: 10),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
+                    ),
+                    Positioned(
+                      top: 10,
+                      right: 10,
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 12, vertical: 7),
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 32, 32, 32),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: const Row(
+                          mainAxisSize: MainAxisSize.min,
                           children: [
-                            Positioned(
-                              top: 10, // กำหนดระยะห่างจากด้านบน
-                              left: 10, // กำหนดระยะห่างจากด้านซ้าย
-                              child: Container(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 12, vertical: 7),
-                                decoration: BoxDecoration(
-                                  color:
-                                      const Color.fromARGB(255, 255, 206, 206),
-                                  borderRadius: BorderRadius.circular(20),
-                                ),
-                                child: const Row(
-                                  mainAxisSize: MainAxisSize
-                                      .min, // กำหนดขนาดของ Row ให้พอดีกับเนื้อหา
-                                  children: [
-                                    Icon(
-                                      Icons.diamond_outlined,
-                                      size: 15,
-                                      color: Color.fromARGB(255, 225, 58, 58),
-                                    ),
-                                    SizedBox(
-                                        width:
-                                            5), // เพิ่มช่องว่างระหว่างไอคอนและข้อความ
-                                    Text(
-                                      '500 Points',
-                                      style: TextStyle(
-                                        fontSize: 12,
-                                        fontWeight: FontWeight.bold,
-                                        color: Color.fromARGB(255, 225, 58, 58),
-                                      ),
-                                    ),
-                                  ],
-                                ),
+                            Icon(
+                              Icons.diamond_outlined,
+                              size: 15,
+                              color: Color.fromARGB(255, 222, 222, 222),
+                            ),
+                            SizedBox(
+                                width:
+                                    5), // เพิ่มช่องว่างระหว่างไอคอนและข้อความ
+                            Text(
+                              '1250 Points',
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromARGB(255, 233, 233, 233),
                               ),
                             ),
                           ],
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
+                const SizedBox(height: 10),
                 const HomeRoutingPanel(),
-                const SizedBox(
-                  height: 20,
-                ),
+                const SizedBox(height: 20),
                 const Row(
                   children: [
                     Text(
@@ -138,9 +140,7 @@ class HomePage extends StatelessWidget {
                   ],
                 ),
                 const GoalsList(),
-                const SizedBox(
-                  height: 30,
-                ),
+                const SizedBox(height: 30),
               ],
             ),
           ),
