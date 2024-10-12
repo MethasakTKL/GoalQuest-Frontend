8920f7b2f73cd961c39e161c41a0ce1e280bc2f4
MethasakTKL <methasak.tkl@outlook.com>
2024-10-13 01:24:24 +0700

    [Goal] Count Goal complete and in Progress , Show "no goal item"

--- lib/models/goal_model.dart
+++ lib/models/goal_model.dart
@@ -1,24 +1,25 @@
 import 'package:equatable/equatable.dart';
 
-class GoalModel extends Equatable{
+class GoalModel extends Equatable {
   final int goalId;
   final String goalTitle;
   final String goalDescription;
   final double goalProgressPercent;
-
+  final bool isCompleted;
 
   const GoalModel({
     required this.goalId,
     required this.goalTitle,
     required this.goalDescription,
     this.goalProgressPercent = 0,
-   
+    this.isCompleted = false,
   });
 
-  @override  List<Object?> get props => [
-    goalId,
-    goalTitle,
-    goalDescription,
-    goalProgressPercent,
-  ];
+  @override
+  List<Object?> get props => [
+        goalId,
+        goalTitle,
+        goalDescription,
+        goalProgressPercent,
+      ];
 }
--- lib/pages/goals/goals_list.dart
+++ lib/pages/goals/goals_list.dart
@@ -16,108 +16,120 @@ class GoalsList extends StatelessWidget {
       child: Column(
         children: [
           const SizedBox(height: 10),
-          ...goals.map((goal) {
-            final goalTasks =
-                tasks.where((task) => task.goalId == goal.goalId).toList();
-            // นับจำนวน Task ทั้งหมด
-            final totalTasks = goalTasks.length;
+          if (goals.isEmpty) // เช็คว่ารายการ goals ว่างไหม
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'No Goals item',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            )
+          else
+            ...goals.map((goal) {
+              final goalTasks =
+                  tasks.where((task) => task.goalId == goal.goalId).toList();
+              // นับจำนวน Task ทั้งหมด
+              final totalTasks = goalTasks.length;
 
-            // นับจำนวน Task ที่เสร็จแล้ว
-            final completedTasks = goalTasks.where((task) {
-              final duration = task.endDate.difference(task.startDate);
-              final repeatCount = duration.inDays ~/ (task.repeatDays ?? 1);
-              return task.taskCount >= repeatCount;
-            }).length;
+              // นับจำนวน Task ที่เสร็จแล้ว
+              final completedTasks = goalTasks.where((task) {
+                final duration = task.endDate.difference(task.startDate);
+                final repeatCount = duration.inDays ~/ (task.repeatDays ?? 1);
+                return task.taskCount >= repeatCount;
+              }).length;
 
-            // คำนวณ progressPercentage และแปลงเป็น double
-            final progressPercentage =
-                totalTasks > 0 ? (completedTasks / totalTasks).toDouble() : 0.0;
+              // คำนวณ progressPercentage และแปลงเป็น double
+              final progressPercentage = totalTasks > 0
+                  ? (completedTasks / totalTasks).toDouble()
+                  : 0.0;
 
-            return Padding(
-              padding: const EdgeInsets.symmetric(vertical: 5.0),
-              child: GoalCard(
-                taskTitle: goal.goalTitle,
-                taskProgress: "$completedTasks/$totalTasks Tasks",
-                progressPercentage: progressPercentage, // ส่งค่าที่คำนวณแล้ว
-                onTap: () {
-                  Navigator.pushNamed(context, '/tasks',
-                      arguments: goal.goalId);
-                },
-                onDelete: () {
-                  showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        title: const Text('Confirm Delete'),
-                        content: const Text(
-                            'Are you sure you want to delete this goal?'),
-                        backgroundColor: Colors.white,
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context)
-                                  .pop(); // ปิด Dialog ถ้ากด Cancel
-                            },
-                            child: const Text(
-                              'Cancel',
-                              style: TextStyle(
-                                  color: Color.fromARGB(255, 60, 60, 60)),
-                            ),
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              // ส่ง Event DeleteGoalEvent ไปที่ GoalBloc พร้อม goalId ที่จะลบ
-                              context
-                                  .read<GoalBloc>()
-                                  .add(DeleteGoalEvent(goal.goalId));
-                              Navigator.of(context)
-                                  .pop(); // ปิด Dialog หลังจากลบ
-                            },
-                            style: TextButton.styleFrom(
-                              backgroundColor:
-                                  const Color.fromARGB(255, 149, 47, 47),
-                            ),
-                            child: const Padding(
-                              padding: EdgeInsets.only(left: 10, right: 10),
-                              child: Text(
-                                'Delete',
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5.0),
+                child: GoalCard(
+                  taskTitle: goal.goalTitle,
+                  taskProgress: "$completedTasks/$totalTasks Tasks",
+                  progressPercentage: progressPercentage, // ส่งค่าที่คำนวณแล้ว
+                  onTap: () {
+                    Navigator.pushNamed(context, '/tasks',
+                        arguments: goal.goalId);
+                  },
+                  onDelete: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: const Text('Confirm Delete'),
+                          content: const Text(
+                              'Are you sure you want to delete this goal?'),
+                          backgroundColor: Colors.white,
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context)
+                                    .pop(); // ปิด Dialog ถ้ากด Cancel
+                              },
+                              child: const Text(
+                                'Cancel',
                                 style: TextStyle(
-                                    color: Color.fromARGB(255, 255, 255, 255)),
+                                    color: Color.fromARGB(255, 60, 60, 60)),
                               ),
                             ),
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                },
+                            TextButton(
+                              onPressed: () {
+                                // ส่ง Event DeleteGoalEvent ไปที่ GoalBloc พร้อม goalId ที่จะลบ
+                                context
+                                    .read<GoalBloc>()
+                                    .add(DeleteGoalEvent(goal.goalId));
+                                Navigator.of(context)
+                                    .pop(); // ปิด Dialog หลังจากลบ
+                              },
+                              style: TextButton.styleFrom(
+                                backgroundColor:
+                                    const Color.fromARGB(255, 149, 47, 47),
+                              ),
+                              child: const Padding(
+                                padding: EdgeInsets.only(left: 10, right: 10),
+                                child: Text(
+                                  'Delete',
+                                  style: TextStyle(
+                                      color:
+                                          Color.fromARGB(255, 255, 255, 255)),
+                                ),
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  onEdit: () {
+                    String goalTitle = goal.goalTitle;
+                    String goalDescription = goal.goalDescription;
+                    int goalId = goal.goalId;
 
-                onEdit: () {
-                  String goalTitle = goal.goalTitle;
-                  String goalDescription = goal.goalDescription;
-                  int goalId = goal.goalId;
-
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return EditGoalDialog(
-                        initialTitle: goalTitle,
-                        initialDescription: goalDescription,
-                        goalId: goalId,
-                        onSave: (updatedTitle, updatedDescription) {
-                          context.read<GoalBloc>().add(EditGoalEvent(
-                                goalId: goalId,
-                                updatedTitle: updatedTitle,
-                                updatedDescription: updatedDescription,
-                              )); // เรียกใช้ Edit Event
-                        },
-                      );
-                    },
-                  );
-                },
-              ),
-            );
-          }).toList(),
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return EditGoalDialog(
+                          initialTitle: goalTitle,
+                          initialDescription: goalDescription,
+                          goalId: goalId,
+                          onSave: (updatedTitle, updatedDescription) {
+                            context.read<GoalBloc>().add(EditGoalEvent(
+                                  goalId: goalId,
+                                  updatedTitle: updatedTitle,
+                                  updatedDescription: updatedDescription,
+                                )); // เรียกใช้ Edit Event
+                          },
+                        );
+                      },
+                    );
+                  },
+                ),
+              );
+            }).toList(),
         ],
       ),
     );
